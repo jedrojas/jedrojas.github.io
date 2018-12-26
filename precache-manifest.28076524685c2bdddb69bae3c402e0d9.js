0a6ad175b12dafa8d3a5b296e68203f185648498
jedrojas <jprojas15@gmail.com>
2018-12-26 02:46:06 -0500

    Updates

---
+++ precache-manifest.28076524685c2bdddb69bae3c402e0d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4f72dee49da0517615dc510dbae634",
+    "url": "/jedrojas.github.io/static/media/headshot.be4f72de.jpeg"
+  },
+  {
+    "revision": "77a7042836d891d56cc1",
+    "url": "/jedrojas.github.io/static/js/runtime~main.77a70428.js"
+  },
+  {
+    "revision": "e46845870f0e3cb5fc54",
+    "url": "/jedrojas.github.io/static/js/main.e4684587.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/jedrojas.github.io/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "e46845870f0e3cb5fc54",
+    "url": "/jedrojas.github.io/static/css/main.658237c2.chunk.css"
+  },
+  {
+    "revision": "7bd188abe8d9a19a59dcf4ddb8f6f881",
+    "url": "/jedrojas.github.io/index.html"
+  }
+];
