0108834a8e260a106fa0a24b0af26fa998ad3be8
jedrojas <jprojas15@gmail.com>
2019-01-18 23:08:14 -0500

    Updates

---
+++ precache-manifest.258b258a7169ab8a1afa2de96ef96c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4f72dee49da0517615dc510dbae634",
+    "url": "/static/media/headshot.be4f72de.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2de2a94747af4196b3b0",
+    "url": "/static/js/main.2de2a947.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "2de2a94747af4196b3b0",
+    "url": "/static/css/main.bc435e2f.chunk.css"
+  },
+  {
+    "revision": "d3bf64210a7249744b47fbd74c3a4d4c",
+    "url": "/index.html"
+  }
+];
